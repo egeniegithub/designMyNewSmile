451ee3eba1debe7b01a6ffabe7ed8bac18aaa575
Egenie Next <egenie.apple@gmail.com>
2020-08-31 12:51:23 +0500

    add image comparison featrure

--- src/containers/SmileDesignDetail/index.js
+++ src/containers/SmileDesignDetail/index.js
@@ -1,38 +1,100 @@
-import React from "react";
-import { View, Text, ScrollView, Image } from "react-native";
+
 import styles from './style';
 import CustomHeader from "../../components/CustomHeader";
 import CustomButton from '../../components/CustomButton';
 
 
+// function SmileDesignDetail(props) {
 
-function SmileDesignDetail(props) {
+//     function onPressMenuIcon() {
+//         props.navigation.toggleDrawer();
+//     }
 
-    function onPressMenuIcon() {
-        props.navigation.toggleDrawer();
+//     return (
+//         <View style={{ flex: 1 }}>
+//             <CustomHeader
+//                 title="Design Detail"
+//                 leftIcon="menu"
+//                 onPress={onPressMenuIcon}
+//             />
+//             <View >
+//                 <Image
+//                     source={require("../../assets/splash.png")}
+//                     style={styles.image}
+//                 />
+
+//                 <Image
+//                     source={require("../../assets/pic1.jpeg")}
+//                     style={[styles.image, {opacity: 0.1}]}
+//                 />
+//             </View>
+
+//         </View >
+//     )
+// }
+
+// export default SmileDesignDetail;
+
+import React, { Component } from 'react';
+import { Text, StyleSheet, View, Dimensions, Image, Platform, ScrollView } from 'react-native';
+import Compare, { Before, After, DefaultDragger, Dragger } from '@malik.aliyev.94/react-native-before-after-slider';
+
+const deviceWidth = Dimensions.get("window").width;
+const deviceHeight = Dimensions.get("window").height;
+
+export default class SmileDesignDetail extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrollEnabled: true,
     }
+  }
 
+  onMoveStart = () => {
+    this.setState({ scrollEnabled: false });
+  }
+
+  onMoveEnd = () => {
+    this.setState({ scrollEnabled: true });
+  }
+
+  renderCompareImages = () => {
+
+  }
+
+  render() {
     return (
-        <View style={{ flex: 1 }}>
-            <CustomHeader
-                title="Design Detail"
-                leftIcon="menu"
-                onPress={onPressMenuIcon}
-            />
-            <View >
-                <Image
-                    source={require("../../assets/splash.png")}
-                    style={styles.image}
-                />
+      <View style={{ flex: 1 }}>
+        <CustomHeader
+          title="Design Detail"
+          leftIcon="menu"
+          onPress={() => { alert('here') }}
+        />
 
-                <Image
-                    source={require("../../assets/pic1.jpeg")}
-                    style={[styles.image, {opacity: 0.1}]}
-                />
-            </View>
-
-        </View >
-    )
+        <ScrollView style={{ marginTop: 50 }} scrollEnabled={this.state.scrollEnabled}>
+          <Compare initial={deviceWidth / 2} draggerWidth={50} onMoveStart={this.onMoveStart} onMoveEnd={this.onMoveEnd}>
+            <Before>
+              <Image source={require('../../assets/splash.png')} style={{ width: deviceWidth, height: deviceWidth / 2 }} />
+            </Before>
+            <After>
+              <Image source={require('../../assets/pic1.jpeg')} style={{ width: deviceWidth, height: deviceWidth / 2 }} />
+            </After>
+            <DefaultDragger />
+          </Compare>
+          <View style={{marginTop: 30}}>
+            <Compare initial={deviceWidth / 2} draggerWidth={50} onMoveStart={this.onMoveStart} onMoveEnd={this.onMoveEnd} >
+              <Before>
+                <Image source={require('../../assets/splash.png')} style={{ width: deviceWidth, height: deviceWidth / 2 }} />
+              </Before>
+              <After>
+                <Image source={require('../../assets/pic1.jpeg')} style={{ width: deviceWidth, height: deviceWidth / 2 }} />
+              </After>
+              <DefaultDragger />
+            </Compare>
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
 }
-
-export default SmileDesignDetail;
