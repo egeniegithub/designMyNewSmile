94dcaa50d5c798de7e4de6198a346866fddf0ce4
Egenie Next <egenie.apple@gmail.com>
2020-09-04 18:31:42 +0500

    show clinic design in smile design

--- src/components/SmilesScreen/index.js
+++ src/components/SmilesScreen/index.js
@@ -4,18 +4,21 @@ import styles from './style';
 import { CheckBox, } from 'native-base';
 import colors from '../../Theme/color';
 import CustomButton from '../CustomButton';
+import { connect } from 'react-redux';
+import { actions } from '../../redux/actions/UserAction';
 
 function SmilesScreen(props) {
+    const { clinic_designs } = props.userObject;
 
-    function SingleSimle() {
+    function SingleSimle(data) {
         return (
             <View style={styles.singleSmileContainer}>
                 <View style={styles.textWithCheckBoxContainer}>
                     <CheckBox checked={true} color={colors.Green} style={styles.checkBox} />
-                    <Text style={styles.designText}>Design #1</Text>
+                    <Text style={styles.designText}>{`Design #${data.number}`}</Text>
                 </View>
                 <Image
-                    source={require("../../assets/splash.png")}
+                    source={{ uri: data.picture }}
                     style={styles.designImage}
                 />
             </View>
@@ -26,27 +29,65 @@ function SmilesScreen(props) {
             <Text style={styles.heading}>SELECT YOUR SMILE DESIGN</Text>
             <View style={styles.smilesMainContainer}>
                 <View style={styles.singleRowSmileContainer}>
-                    <SingleSimle />
-                    <SingleSimle />
+                    {
+                        clinic_designs[0] &&
+                        <SingleSimle picture={clinic_designs[0].d_aimage} number={1} />
+                    }
+
+                    {
+                        clinic_designs[1] &&
+                        <SingleSimle picture={clinic_designs[1].d_aimage} number={2} />
+                    }
                 </View>
 
-                <View style={styles.singleRowSmileContainer}>
-                    <SingleSimle />
-                    <SingleSimle />
-                </View>
+                {
+                    clinic_designs[2] &&
+                    <View style={styles.singleRowSmileContainer}>
+                        {
+                            clinic_designs[2] &&
+                            <SingleSimle picture={clinic_designs[2].d_aimage} number={3} />
+                        }
+                        {
+                            clinic_designs[3] &&
+                            <SingleSimle picture={clinic_designs[3].d_aimage} number={4} />
+                        }
+                    </View>
+                }
 
-                <View style={styles.singleRowSmileContainer}>
-                    <SingleSimle />
-                    <SingleSimle />
-                </View>
+                {
+                    clinic_designs[4] &&
+                    <View style={styles.singleRowSmileContainer}>
+                        {
+                            clinic_designs[4] &&
+                            <SingleSimle picture={clinic_designs[4].d_aimage} number={5} />
+                        }
+                        {
+                            clinic_designs[5] &&
+                            <SingleSimle picture={clinic_designs[5].d_aimage} number={6} />
+                        }
+                    </View>
+                }
+                <CustomButton
+                    text={"Select Design"}
+                    style={styles.customButton}
+                    onPress={() => props.navigation.navigate('GetAppointment')}
+                />
             </View>
-            <CustomButton
-                text={"Select Design"}
-                style={styles.customButton}
-                onPress={() => props.navigation.navigate('GetAppointment')}
-            />
+
         </ScrollView>
     );
 }
 
-export default SmilesScreen;
+const mapStateToProps = state => {
+    return {
+        userObject: state.user.userObject,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        getProfile: () => dispatch(actions.getProfile()),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SmilesScreen);
